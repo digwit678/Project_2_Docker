edd80edb84f8e4c9faf1fe74589822363029c9de
digwit678 <nico@previtali.net>
2023-11-12 22:23:45 +0100

    Update start.sh
    added docker playground version that does not rely on datasets package to save memory space

--- start.sh
+++ start.sh
@@ -9,8 +9,12 @@ echo "TensorBoard started."
 
 echo "Starting Python script..."
 
-# Execute Python script
-python DistilBERT_MRPC_Script.py --checkpoint_dir models --learning_rate 5.89e-05 --weight_decay 0.0519 --batch_size 64
+# for task 2 use 
+#python DistilBERT_MRPC_Script.py --checkpoint_dir models --learning_rate 5.89e-05 --weight_decay 0.0519 --batch_size 64
+# for task 3 use
+# Execute Python script without checkpoint_dir argument
+python Task_3_DistilBERT_MRPC_Script.py --learning_rate 5.89e-05 --weight_decay 0.0519 --batch_size 64
+
 
 echo "Python script started"
 
