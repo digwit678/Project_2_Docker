c079f132aa0a5e08d5e863c57b7bfa486bac15e0
Previtali Nico <nico.previtali@stud.hslu.ch>
2023-11-15 01:27:13 +0100

    updated docker desktop setup files

--- docker_setup_desktop_linux.sh
+++ docker_setup_desktop_linux.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check if Docker is running
+if ! docker info >/dev/null 2>&1; then
+    echo "Docker does not seem to be running, please start Docker Desktop, and retry."
+    echo "Press enter to exit..."
+    read
+    exit 1
+fi
+
 echo "Cloning the repository..."
 git clone https://github.com/digwit678/Project_2_Docker.git
 
